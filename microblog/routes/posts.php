6ab3bcde1655a47d3cbe3a9076d7bf349ad7a45d
Ian Murzac <ianik123450909w@gmail.com>
2025-10-26 20:55:29 +0200

    feat(auth): add Laravel Breeze (#3)

---
+++ microblog/routes/posts.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts{post}', [PostController::class, 'show'])->name('posts.show');
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+});
