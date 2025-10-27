6a5021894d07e40b6b7c73c92de52b5143d81ac0
Ian Murzac <ianik123450909w@gmail.com>
2025-10-27 15:52:06 +0200

    feat(posts): add authorization and policies for post management (#5)

--- microblog/app/Providers/AppServiceProvider.php
+++ microblog/app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $policies = [
+        \App\Models\Post::class => \App\Policies\PostPolicy::class,
+    ];
     /**
      * Register any application services.
      */
