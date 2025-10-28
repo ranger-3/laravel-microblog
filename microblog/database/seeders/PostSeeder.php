19f6bdc7a3043487e659fc9b0ffeef69c5971ccf
Ian Murzac <ianik123450909w@gmail.com>
2025-10-28 15:46:42 +0200

    feat(seeders): add role, permission, user and post seeders

---
+++ microblog/database/seeders/PostSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::all()->each(function ($user) {
+
+            Post::factory()->count(fake()->numberBetween(2, 5))->create(['user_id' => $user->id]);
+        });
+    }
+}
